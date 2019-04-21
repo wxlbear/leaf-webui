24e57c3ffc6722fe32efc7ba6ac8458be8681cb3
xiongliu.wu <xiongliu.wu@500px.com>
2019-04-21 20:55:33 +0800

    mod index page

---
+++ src/components/Picture.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="picture-pane">
+    <div class="picture-item">
+        <img src="https://bear-picture.oss-cn-beijing.aliyuncs.com/life/WechatIMG1.jpeg?Expires=1555854709&OSSAccessKeyId=TMP.AgHJPwQHYrj382sffte5Tol7BgDxi0hiHqMkr9NNQ7Pm0Teu3cEZ0HFxr9k5AAAwLAIUEY6eclRYT8_XXy-_DBohd-_kyyUCFDvyla-_uMKzOzE4Xdmo6c2cW34r&Signature=%2FSvLt9CE6IoT%2FpHD6j9%2FugLh1OM%3D" alt="">
+    </div>
+    <div class="picture-item">
+        <img src="https://bear-picture.oss-cn-beijing.aliyuncs.com/life/psb%20%281%29.jpeg?Expires=1555854747&OSSAccessKeyId=TMP.AgHJPwQHYrj382sffte5Tol7BgDxi0hiHqMkr9NNQ7Pm0Teu3cEZ0HFxr9k5AAAwLAIUEY6eclRYT8_XXy-_DBohd-_kyyUCFDvyla-_uMKzOzE4Xdmo6c2cW34r&Signature=Ui1dO3f8cQ4ONKxjY%2BTRTbYCC3o%3D" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Picture',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
