56545e1695beff372af7fd385ffea825da21ba9e
xiongliu.wu <xiongliu.wu@500px.com>
2019-04-21 23:20:47 +0800

    format

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,21 +1,52 @@
 <template>
   <div class="picture-pane">
-    <div class="picture-item">
-        <img src="https://bear-picture.oss-cn-beijing.aliyuncs.com/life/WechatIMG1.jpeg?Expires=1555854709&OSSAccessKeyId=TMP.AgHJPwQHYrj382sffte5Tol7BgDxi0hiHqMkr9NNQ7Pm0Teu3cEZ0HFxr9k5AAAwLAIUEY6eclRYT8_XXy-_DBohd-_kyyUCFDvyla-_uMKzOzE4Xdmo6c2cW34r&Signature=%2FSvLt9CE6IoT%2FpHD6j9%2FugLh1OM%3D" alt="">
-    </div>
-    <div class="picture-item">
-        <img src="https://bear-picture.oss-cn-beijing.aliyuncs.com/life/psb%20%281%29.jpeg?Expires=1555854747&OSSAccessKeyId=TMP.AgHJPwQHYrj382sffte5Tol7BgDxi0hiHqMkr9NNQ7Pm0Teu3cEZ0HFxr9k5AAAwLAIUEY6eclRYT8_XXy-_DBohd-_kyyUCFDvyla-_uMKzOzE4Xdmo6c2cW34r&Signature=Ui1dO3f8cQ4ONKxjY%2BTRTbYCC3o%3D" alt="">
+    <div class="picture-item"
+         v-for="image in images"
+         v-bind:key="image.id">
+      <img v-bind:src="image.url" />
+
     </div>
   </div>
 </template>
 
 <script>
+import { get } from "../utils/request.js";
+
 export default {
-  name: 'Picture',
+  name: "Picture",
   props: {
     msg: String
+  },
+  data: function() {
+    return {
+      images: []
+    };
+  },
+  mounted: function() {
+    console.log("mounted");
+    get("/pictures/pager", {}, list => {
+      var records = list.records;
+      records = [
+        {
+          id: 1,
+          url:
+            "https://bear-picture.oss-cn-beijing.aliyuncs.com/life/WechatIMG1.jpeg?Expires=1555854709&OSSAccessKeyId=TMP.AgHJPwQHYrj382sffte5Tol7BgDxi0hiHqMkr9NNQ7Pm0Teu3cEZ0HFxr9k5AAAwLAIUEY6eclRYT8_XXy-_DBohd-_kyyUCFDvyla-_uMKzOzE4Xdmo6c2cW34r&Signature=%2FSvLt9CE6IoT%2FpHD6j9%2FugLh1OM%3D"
+        },
+        {
+          id: 2,
+          url:
+            "https://bear-picture.oss-cn-beijing.aliyuncs.com/life/psb%20%281%29.jpeg?Expires=1555854747&OSSAccessKeyId=TMP.AgHJPwQHYrj382sffte5Tol7BgDxi0hiHqMkr9NNQ7Pm0Teu3cEZ0HFxr9k5AAAwLAIUEY6eclRYT8_XXy-_DBohd-_kyyUCFDvyla-_uMKzOzE4Xdmo6c2cW34r&Signature=Ui1dO3f8cQ4ONKxjY%2BTRTbYCC3o%3D"
+        }
+      ];
+      this.images = records.map(item => {
+        return {
+          url: item.url,
+          id: item.id
+        };
+      });
+    });
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ src/utils/request.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+
+// axios.defaults.baseURL = 'http://118.24.64.174';
+axios.defaults.baseURL = 'http://localhost:81/api';
+
+export function get(url, params={}, callback){
+    axios({
+        method:'get',
+        url: url,
+        params:params,
+    }).then(function(response){
+        callback(response.data)
+    }).catch(function (error) {
+        console.log(error);
+      });
+}
+
+export function post(url, data={}, callback){
+    axios({
+        method:'post',
+        url: url,
+        data:data,
+    }).then(function(response){
+        callback(response.data)
+    }).catch(function(error){
+        console.log(error);
+    })
+}
+
+export function remove(url,id, callback){
+    axios({
+        method:'delete',
+        url: url,
+        params:{id:id}
+    }).then(function(response){
+        callback(response.data)
+    }).catch(function(error){
+        console.log(error);
+    })
+}
+
+export function update(url,data={}, callback){
+    axios({
+        method:'put',
+        url: url,
+        data:data
+    }).then(function(response){
+        callback(response.data)
+    }).catch(function(error){
+        console.log(error);
+    })
+}
