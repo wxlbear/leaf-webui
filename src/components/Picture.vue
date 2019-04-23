81064e6f77199d29864d762a64bee6f86ad1c340
xiongliu.wu <xiongliu.wu@500px.com>
2019-04-23 18:12:32 +0800

    mod uri

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -29,12 +29,12 @@ export default {
         {
           id: 1,
           url:
-            "https://bear-picture.oss-cn-beijing.aliyuncs.com/life/WechatIMG1.jpeg?Expires=1555854709&OSSAccessKeyId=TMP.AgHJPwQHYrj382sffte5Tol7BgDxi0hiHqMkr9NNQ7Pm0Teu3cEZ0HFxr9k5AAAwLAIUEY6eclRYT8_XXy-_DBohd-_kyyUCFDvyla-_uMKzOzE4Xdmo6c2cW34r&Signature=%2FSvLt9CE6IoT%2FpHD6j9%2FugLh1OM%3D"
+            "https://bear-picture.oss-cn-beijing.aliyuncs.com/life/WechatIMG1.jpeg"
         },
         {
           id: 2,
           url:
-            "https://bear-picture.oss-cn-beijing.aliyuncs.com/life/psb%20%281%29.jpeg?Expires=1555854747&OSSAccessKeyId=TMP.AgHJPwQHYrj382sffte5Tol7BgDxi0hiHqMkr9NNQ7Pm0Teu3cEZ0HFxr9k5AAAwLAIUEY6eclRYT8_XXy-_DBohd-_kyyUCFDvyla-_uMKzOzE4Xdmo6c2cW34r&Signature=Ui1dO3f8cQ4ONKxjY%2BTRTbYCC3o%3D"
+            "https://bear-picture.oss-cn-beijing.aliyuncs.com/life/psb%20%281%29.jpeg"
         }
       ];
       this.images = records.map(item => {
